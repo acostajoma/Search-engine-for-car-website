e4767e9ed4dbbc262d253804a53cd9051e767c00
Jose Acosta <acostajoma9@gmail.com>
2022-06-14 13:40:31 -0600

    Added text message in case no results are matched, and function that refreshes the HTML once a new field value is provided

--- js/app.js
+++ js/app.js
@@ -26,7 +26,7 @@ const datosBusqueda = {
 
 //Events
 document.addEventListener('DOMContentLoaded', ()=>{
-    mostrarAutos(); //MUESTRA AUTOMOVILES
+    mostrarAutos(autos); //MUESTRA AUTOMOVILES
 
 
     //Llena las opciones de a;os
@@ -36,34 +36,42 @@ document.addEventListener('DOMContentLoaded', ()=>{
 //eventListeners
 marca.addEventListener('change', e =>{
     datosBusqueda.marca = e.target.value;
+    filtrarAuto();
 });
 
 year.addEventListener('change', e =>{
     datosBusqueda.year = e.target.value;
+    filtrarAuto();
 });
 
 minimo.addEventListener('change', e =>{
     datosBusqueda.minimo = e.target.value;
+    filtrarAuto();
 });
 
 maximo.addEventListener('change', e =>{
     datosBusqueda.maximo = e.target.value;
+    filtrarAuto();
 });
 
 puertas.addEventListener('change', e =>{
     datosBusqueda.puertas = e.target.value;
+    filtrarAuto();
 });
 
 transmision.addEventListener('change', e =>{
     datosBusqueda.transmision = e.target.value;
+    filtrarAuto();
 });
 
 color.addEventListener('change', e =>{
     datosBusqueda.color = e.target.value;
+    filtrarAuto();
 });
 
 //Functions
-function mostrarAutos(){
+function mostrarAutos(autos){
+    limpiarHTML(); //elimina el html previo
     autos.forEach(auto => {
 
         const { marca, modelo, year, puertas, transmision, precio, color } = auto;
@@ -76,6 +84,12 @@ function mostrarAutos(){
     });
 }
 
+//Limpiar HTML
+function limpiarHTML (){
+    while (resultado.firstChild){
+        resultado.removeChild(resultado.firstChild);
+    }
+}
 
 //Genera los a;os del select
 
@@ -86,5 +100,77 @@ function llenarSelect(){
         opcion.textContent = i;
         year.appendChild(opcion);
     }
+}
 
+function filtrarAuto(){
+    const resultado = autos.filter(filtrarMarca).filter(filtrarYear).filter(filtrarMinimo).filter(filtrarMaximo).filter( filtrarPuertas).filter(filtrarTransmision).filter(filtrarColor);
+    // console.log(resultado);
+    if (resultado.length){
+        mostrarAutos(resultado);
+    } else {
+        noResultado();
+    }
+}
+
+function noResultado(){
+    const noResultado = document.createElement('div');
+    noResultado.classList.add('alerta', 'error');
+    noResultado.textContent = 'No hay resultados';
+    resultado.appendChild(noResultado);
+}
+
+function filtrarMarca(auto){
+    const {marca} = datosBusqueda;
+    if ( marca ){
+        return auto.marca === marca;
+    }
+    return auto;
+}
+
+function filtrarYear(auto) {
+    const {year} = datosBusqueda;
+    if ( year ){
+        return auto.year === parseInt(year);
+    }
+    return auto;
+}
+
+function filtrarMinimo(auto){
+    const {minimo} = datosBusqueda;
+    if ( minimo ){
+        return auto.precio >= minimo;
+    }
+    return auto;
+}
+
+function filtrarMaximo(auto){
+    const {maximo} = datosBusqueda;
+    if ( maximo ){
+        return auto.precio <= maximo;
+    }
+    return auto;
+}
+
+function filtrarPuertas(auto) {
+    const {puertas} = datosBusqueda;
+    if ( puertas ){
+        return auto.puertas === parseInt(puertas);
+    }
+    return auto;
+}
+
+function filtrarTransmision(auto) {
+    const {transmision} = datosBusqueda;
+    if ( transmision ){
+        return auto.transmision === transmision;
+    }
+    return auto;
+}
+
+function filtrarColor(auto) {
+    const {color} = datosBusqueda;
+    if ( color ){
+        return auto.color === color;
+    }
+    return auto;
 }
